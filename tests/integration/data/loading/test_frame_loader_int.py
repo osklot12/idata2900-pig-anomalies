fa034622537a4ebc719b0d7b1ef4c6b5664138cf
Oskar <osklot12@gmail.com>
2025-02-24 20:16:09 +0100

    Fixed incorrect use of bucket configurations in test_frame_loader.int

--- tests/integration/data/loading/test_frame_loader_int.py
+++ tests/integration/data/loading/test_frame_loader_int.py
@@ -5,18 +5,15 @@ import pytest
 from src.auth.gcp_auth_service import GCPAuthService
 from src.data.loading.frame_loader import FrameLoader
 from src.data.loading.gcp_data_loader import GCPDataLoader
-from tests.conftest import get_test_path
+from src.utils.norsvin_bucket_parser import NorsvinBucketParser
+from tests.utils.constants.sample_bucket_files import SampleBucketFiles
 
-TEST_CREDENTIALS_PATH = get_test_path(".secrets/service-account.json")
-TEST_BUCKET = "norsvin-g2b-behavior-prediction"
-TEST_VIDEO_PREFIX = "g2b_behaviour/images/"
-TEST_VIDEO_NAME = "avd13_cam1_20220314072829_20220314073013_fps2.0.mp4"
 
 @pytest.fixture(scope="module")
 def gcp_data_loader():
     """Fixture to provide a GCPDataLoader instance."""
-    auth_service = GCPAuthService(credentials_path=TEST_CREDENTIALS_PATH)
-    return GCPDataLoader(bucket_name=TEST_BUCKET, auth_service=auth_service)
+    auth_service = GCPAuthService(credentials_path=NorsvinBucketParser.CREDENTIALS_PATH)
+    return GCPDataLoader(bucket_name=NorsvinBucketParser.BUCKET_NAME, auth_service=auth_service)
 
 @pytest.fixture
 def mock_callback():
@@ -28,7 +25,7 @@ def test_load_frames(gcp_data_loader, mock_callback):
     """Integration test: Ensures that frames are loaded and processed correctly."""
 
     # arrange
-    video_blob_name = f"{TEST_VIDEO_PREFIX}{TEST_VIDEO_NAME}"
+    video_blob_name = NorsvinBucketParser.get_video_blob_name(SampleBucketFiles.SAMPLE_VIDEO_FILE)
     frame_loader = FrameLoader(
         data_loader=gcp_data_loader,
         callback=mock_callback,
