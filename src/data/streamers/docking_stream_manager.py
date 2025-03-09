e4f6b3daa34c8f0c0f8182abbdc5e0d786481178
Oskar <osklot12@gmail.com>
2025-03-09 14:11:56 +0100

    Implemented CommandExecutor and unit tests

---
+++ src/data/streamers/docking_stream_manager.py
@@ -0,0 +1,76 @@
+import threading
+import uuid
+from typing import Type, Dict
+
+from src.data.streamers.stream_manager import StreamManager
+from src.data.streamers.streamer_provider import StreamerProvider
+from src.data.streamers.streamer import Streamer
+
+
+class DockingStreamManager(StreamManager):
+    """
+    A stream manager effective for large sets of finite streams, maintaining a constant number of streams at all time.
+    The Streamers "dock" the manager, before leaving and making space for the next streamer.
+    """
+
+    def __init__(self, streamer_provider: StreamerProvider, n_streamers: int):
+        """
+        Initializes a new instance of the DockingStreamManager class.
+
+        Args:
+            streamer_provider (Type[StreamerProvider]): Provides streamers.
+            n_streamers (int): The number of streamers to maintain at all times.
+        """
+        self.streamer_provider = streamer_provider
+        self.n_streamers = n_streamers
+        self.streamers: Dict[str, Streamer] = {}
+        self.lock = threading.Lock()
+
+    def run(self):
+        # dock streamers until full or until no more streamers are available
+        while not self._is_full() and self._dock_next_streamer():
+            pass
+
+    def _is_full(self) -> bool:
+        """Indicates whether the manager is full of streamers."""
+        with self.lock:
+           return len(self.streamers) == self.n_streamers
+
+    def stop(self):
+        with self.lock:
+            for streamer in self.streamers.values():
+                streamer.stop()
+            self.streamers.clear()
+
+    def terminate_streamer(self, streamer_id: str):
+        with self.lock:
+            streamer = self.streamers.pop(streamer_id, None)
+            if streamer:
+                streamer.stop()
+            else:
+                raise KeyError(f"Streamer ID '{streamer_id}' not found.")
+
+    def _dock_next_streamer(self) -> bool:
+        """Fetches the next streamer and runs the stream."""
+        result = False
+
+        streamer = self.streamer_provider.get_next_streamer()
+        if streamer:
+            self._add_streamer(streamer)
+            streamer.stream()
+            result = True
+
+        return result
+
+    @staticmethod
+    def _generate_streamer_id() -> str:
+        """
+        Generates a unique streamer identifier.
+        """
+        return str(uuid.uuid4())
+
+    def _add_streamer(self, streamer: Streamer):
+        """Adds a streamer to the manager."""
+        streamer_id = self._generate_streamer_id()
+        with self.lock:
+            self.streamers[streamer_id] = streamer
--- src/data/streamers/ensemble_streamer.py
+++ src/data/streamers/ensemble_streamer.py
@@ -1,58 +1,26 @@
 import threading
 from typing import Tuple
 
-from src.command.command import Command
 from src.data.streamers.streamer import Streamer
 
 
 class EnsembleStreamer(Streamer):
     """A streamer consisting of other streamers."""
 
-    def __init__(self, streamers: Tuple[Streamer, ...], termination_command: Command = None):
+    def __init__(self, streamers: Tuple[Streamer, ...]):
         """
         Initializes the StreamerGroup.
 
         Args:
             streamers (Tuple[Streamer, ...]): The streamers belonging to the group.
-            termination_command (Command): A optional termination command, executed when all streamers are finished.
         """
         self.streamers = streamers
-        self.termination_command = termination_command
-        self._thread: threading.Thread | None = None
-        self._lock = threading.Lock()
+        self.lock = threading.Lock()
 
-    def stream(self):
-        with self._lock:
-            self._check_thread_safety()
+    def stream(self) -> None:
+        for streamer in self.streamers:
+            streamer.stream()
 
-            for streamer in self.streamers:
-                streamer.stream()
-
-            self._thread = threading.Thread(target=self._wait_worker)
-            self._thread.start()
-
-    def _wait_worker(self):
-        for stream in self.streamers:
-            stream.wait_for_completion()
-
-        if self.termination_command:
-            self.termination_command.execute()
-
-    def stop(self):
-        with self._lock:
-            for stream in self.streamers:
-                stream.stop()
-        self._join_thread()
-
-    def wait_for_completion(self):
-        self._join_thread()
-
-    def _check_thread_safety(self):
-        """Ensures only one instance runs at a time."""
-        if self._thread and self._thread.is_alive():
-            raise RuntimeError("Streamers already running on another thread.")
-
-    def _join_thread(self):
-        if self._thread and self._thread.is_alive():
-            self._thread.join()
-            self._thread = None
+    def stop(self) -> None:
+        for streamer in self.streamers:
+            streamer.stop()
