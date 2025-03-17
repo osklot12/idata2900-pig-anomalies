9374bc8ce85122bc7d91ef6712809387b52d98c9
Oskar <osklot12@gmail.com>
2025-03-17 13:22:48 +0100

    Implemented LazyAnnotation

---
+++ src/data/dataset/entities/lazy_annotation.py
@@ -0,0 +1,23 @@
+from src.data.dataset.entities.annotation import Annotation
+from src.data.loading.loaders.annotation_loader import AnnotationLoader
+
+
+class LazyAnnotation(Annotation):
+    """An annotation that lazy loads the annotation data."""
+
+    def __init__(self, annotation_id: str, annotation_loader: AnnotationLoader):
+        """
+        Initializes a LazyAnnotation instance.
+
+        Args:
+            annotation_id (str): the annotation ID
+            annotation_loader (AnnotationLoader): the annotation loader
+        """
+        self._id = annotation_id
+        self._loader = annotation_loader
+
+    def get_id(self) -> str:
+        return self._id
+
+    def get_data(self) -> dict:
+        return self._loader.load_annotation(self._id)
