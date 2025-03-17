146d15a5e316a075149d2167cd3363a0b4a043c0
Oskar <osklot12@gmail.com>
2025-03-17 13:18:29 +0100

    Implemented Annotation interface

---
+++ src/data/dataset/entities/annotation.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Annotation(ABC):
+    """An interface for annotations."""
+
+    @abstractmethod
+    def get_id(self) -> str:
+        """
+        Returns the annotation ID.
+
+        Returns:
+            str: the annotation ID
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_data(self) -> dict:
+        """
+        Returns the annotation data.
+
+        Returns:
+            dict: the annotation data
+        """
+        raise NotImplementedError
