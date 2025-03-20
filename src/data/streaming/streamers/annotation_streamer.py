c37618721989d74f5685ea589a43bb76640a1849
Oskar <osklot12@gmail.com>
2025-03-20 13:19:15 +0100

    Implemented optional bbox normalization in AnnotationStreamer using dependency injection

--- src/data/streaming/streamers/annotation_streamer.py
+++ src/data/streaming/streamers/annotation_streamer.py
@@ -2,8 +2,10 @@ from abc import abstractmethod
 
 from typing import Callable, Optional
 
+from src.data.dataclasses.bbox_annotation import BBoxAnnotation
 from src.data.dataclasses.frame_annotation import FrameAnnotation
 from src.data.loading.feed_status import FeedStatus
+from src.data.preprocessing.normalization.bbox_normalization_strategy import BBoxNormalizationStrategy
 from src.data.streaming.streamers.streamer import Streamer
 from src.data.streaming.streamers.streamer_status import StreamerStatus
 
@@ -11,28 +13,53 @@ from src.data.streaming.streamers.streamer_status import StreamerStatus
 class AnnotationStreamer(Streamer):
     """A streamer for streaming annotation data."""
 
-    def __init__(self, callback: Callable[[FrameAnnotation], FeedStatus]):
+    def __init__(self, callback: Callable[[FrameAnnotation], FeedStatus], normalizer: Optional[BBoxNormalizationStrategy] = None):
         """
         Initializes a AnnotationStreamer instance.
 
         Args:
             callback (Callable[[Annotation], FeedStatus]): the callback to feed data
+            normalizer (BBoxNormalizationStrategy): the bounding box normalization strategy
         """
         super().__init__()
         self._callback = callback
+        self._normalizer = normalizer
 
     def _stream(self) -> StreamerStatus:
         result = StreamerStatus.COMPLETED
 
         annotation = self._get_next_annotation()
         while annotation is not None and not self._is_requested_to_stop():
-            self._callback(annotation)
+            self._callback(
+                self._normalize_annotation(annotation)
+            )
             annotation = self._get_next_annotation()
 
         if annotation and self._is_requested_to_stop():
             result = StreamerStatus.STOPPED
 
         return result
+
+    def _normalize_annotation(self, annotation: FrameAnnotation) -> FrameAnnotation:
+        """Normalizes an annotation."""
+        if self._normalizer:
+            normalized_bboxes = []
+            for anno_box in annotation.annotations:
+                normalized_bboxes.append(
+                    BBoxAnnotation(
+                        cls=anno_box.cls,
+                        bbox=self._normalizer.normalize_bounding_box(anno_box.bbox)
+                    )
+                )
+
+            annotation = FrameAnnotation(
+                source=annotation.source,
+                index=annotation.index,
+                annotations=normalized_bboxes,
+                end_of_stream=annotation.end_of_stream,
+            )
+
+        return annotation
 
     @abstractmethod
     def _get_next_annotation(self) -> Optional[FrameAnnotation]:
