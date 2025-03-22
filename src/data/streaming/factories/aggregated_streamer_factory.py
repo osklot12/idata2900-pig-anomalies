8f960b8ae267991f3a2511e88fe1e30cf64857cc
Oskar <osklot12@gmail.com>
2025-03-23 00:12:38 +0100

    Implemented AggregatedStreamerFactory and unit test suite for it

---
+++ src/data/streaming/factories/aggregated_streamer_factory.py
@@ -0,0 +1,25 @@
+from typing import Callable
+
+from src.data.dataclasses.instance import Instance
+from src.data.loading.feed_status import FeedStatus
+from src.data.streaming.factories.streamer_factory import StreamerFactory
+from src.data.streaming.factories.streamer_pair_factory import StreamerPairFactory
+from src.data.streaming.streamers.aggregated_streamer import AggregatedStreamer
+
+
+class AggregatedStreamerFactory(StreamerFactory):
+    """A factory for creating AggregatedStreamer instances."""
+
+    def __init__(self, streamer_pair_factory: StreamerPairFactory, callback: Callable[[Instance], FeedStatus]):
+        """
+        Initializes an AggregatedStreamer instance.
+
+        Args:
+            streamer_pair_factory (StreamerPairFactory): a factory for creating streamer pairs
+            callback (Callable[[Instance], FeedStatus]): the callback to provide to the aggregated streamers
+        """
+        self._streamer_pair_factory = streamer_pair_factory
+        self._callback = callback
+
+    def get_next_streamer(self) -> AggregatedStreamer:
+        return AggregatedStreamer(self._streamer_pair_factory, self._callback)
