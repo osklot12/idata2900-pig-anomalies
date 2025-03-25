b14fa79a8e926cab0a1d21fe33f25e31223fda3c
Oskar <osklot12@gmail.com>
2025-03-25 21:59:40 +0100

    Documented all dataclasses

--- src/data/dataclasses/frame.py
+++ src/data/dataclasses/frame.py
@@ -7,7 +7,15 @@ from src.data.dataclasses.source_metadata import SourceMetadata
 
 @dataclass(frozen=True)
 class Frame:
-    """Represents a single video frame containing raw pixel data."""
+    """
+    Represents a single video frame containing raw pixel data.
+
+    Attributes:
+        source (SourceMetadata): the source metadata
+        index (int): the frame index within its source
+        data (np.ndarray): the raw pixel data
+        end_of_stream (bool): whether the frame is at the end of the stream
+    """
     source: SourceMetadata
     index: int
     data: np.ndarray
--- src/data/dataclasses/frame_annotations.py
+++ src/data/dataclasses/frame_annotations.py
@@ -7,7 +7,15 @@ from src.data.dataclasses.source_metadata import SourceMetadata
 
 @dataclass(frozen=True)
 class FrameAnnotations:
-    """Represents annotations associated with a specific frame in a video stream."""
+    """
+    Represents annotations associated with a specific frame in a video stream.
+
+    Attributes:
+        source (SourceMetadata): the source metadata
+        index (int): the frame index within its source
+        annotations (List[AnnotatedBBox]): the annotations associated with the frame
+        end_of_stream (bool): whether the frame is at the end of the stream
+    """
     source: SourceMetadata
     index: int
     annotations: List[AnnotatedBBox]
