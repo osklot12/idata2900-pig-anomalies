9d7067d2114e9b8b1a468dc4be41de05605943c0
EpicDome <chris8br@gmail.com>
2025-03-28 15:18:57 +0100

    Refactored batch and instance request and response to be handled generic instead of via put pop

--- src/network/client/client_network.py
+++ src/network/client/client_network.py
@@ -1,18 +1,33 @@
 import socket
+import struct
 from typing import TypeVar
 from src.network.messages.requests.request import Request
 from src.network.messages.response.response import Response
-from src.network.message_handler import MessageHandler
+from src.network.messages.serialization.message_deserializer import MessageDeserializer
+from src.network.messages.serialization.message_serializer import MessageSerializer
+from src.network.network_config import NETWORK_MSG_LEN_FORMAT, NETWORK_SERVER_PORT, NETWORK_MSG_LEN_FIELD_BYTES
 
 T = TypeVar("T", bound=Response)
 
 
 class NetworkClient:
-    def __init__(self, ip: str, port: int):
+    def __init__(self, ip: str, message_reader: MessageReader, message_serializer: MessageSerializer, message_deserializer: MessageDeserializer):
+        """
+        Initialize a network client
+
+        Args:
+            message_reader (MessageReader): Message reader
+            message_serializer (MessageSerializer): Message serializer
+            message_deserializer (MessageDeserializer): Message deserializer
+        """
         self._ip = ip
-        self._port = port
+        self._port = NETWORK_SERVER_PORT
         self._socket: socket.socket | None = None
-        self._msg_len = NETWORK_MSG_LEN_BYTES
+        self._serializer = message_serializer
+        self._deserializer = message_deserializer
+        self._msg_len = NETWORK_MSG_LEN_FORMAT
+        self._msg_bytes = NETWORK_MSG_LEN_FIELD_BYTES
+        self._msg_reader = MessageReader
 
     def connect(self) -> None:
         """
@@ -37,9 +52,12 @@ class NetworkClient:
         if self._socket is None:
             raise RuntimeError("Not connected")
 
-        MessageHandler.send_message(self._socket, request)
+            # --- Serialize ---
+        data = self._serializer.serialize(request)
+        length = struct.pack(NETWORK_MSG_LEN_FORMAT, len(data))
+        self._socket.sendall(length + data)
 
-        response: T = MessageHandler.receive_message(self._socket)
+        response: T = self._msg_reader.read_message(self._socket.makefile('rb'))
         return response
 
     def close(self) -> None:
