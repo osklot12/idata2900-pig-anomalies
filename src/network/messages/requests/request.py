21fdc1dc71bbf527569b8502906e51296af37564
Oskar <osklot12@gmail.com>
2025-03-28 16:53:47 +0100

    Refactored request and response interfaces, and the corresponding implementations

--- src/network/messages/requests/request.py
+++ src/network/messages/requests/request.py
@@ -3,16 +3,24 @@ from typing import TypeVar, Generic
 
 from src.network.messages.message import Message
 from src.network.messages.response.response import Response
-from src.network.server.context.server_context import ServerContext
 
-C = TypeVar('C')
+# server-side context
+S = TypeVar('S')
+
+# return-type of response
 R = TypeVar('R')
 
-class Request(Message, Generic[C, R]):
-    """An interface for requests."""
+class Request(Message, Generic[S, R]):
+    """
+    An interface for requests.
+
+    Type Parameters:
+        S (ServerContext): context used on the server side to execute the request
+        R (ReturnType): the final result produced when the response is executed
+    """
 
     @abstractmethod
-    def execute(self, context: C) -> Response[?, R]:
+    def execute(self, context: S) -> Response[R]:
         """
         Executes the request.
 
