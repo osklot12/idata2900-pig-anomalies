637481d86cfe79c72e0b4c0fc9b039add6443622
EpicDome <chris8br@gmail.com>
2025-03-28 16:13:56 +0100

    Some refactoring done with context

--- src/network/client/client_network.py
+++ src/network/client/client_network.py
@@ -1,17 +1,20 @@
 import socket
 import struct
 from typing import TypeVar
+
+from src.network.messages.readers.message_reader import MessageReader
+from src.network.messages.readers.stream_message_reader import StreamMessageReader
 from src.network.messages.requests.request import Request
 from src.network.messages.response.response import Response
 from src.network.messages.serialization.message_deserializer import MessageDeserializer
 from src.network.messages.serialization.message_serializer import MessageSerializer
-from src.network.network_config import NETWORK_MSG_LEN_FORMAT, NETWORK_SERVER_PORT, NETWORK_MSG_LEN_FIELD_BYTES
+from src.network.network_config import NETWORK_MSG_LEN_FORMAT, NETWORK_SERVER_PORT
 
 T = TypeVar("T", bound=Response)
 
 
 class NetworkClient:
-    def __init__(self, ip: str, message_reader: MessageReader, message_serializer: MessageSerializer, message_deserializer: MessageDeserializer):
+    def __init__(self, ip: str, message_serializer: MessageSerializer, message_deserializer: MessageDeserializer):
         """
         Initialize a network client
 
@@ -23,11 +26,10 @@ class NetworkClient:
         self._ip = ip
         self._port = NETWORK_SERVER_PORT
         self._socket: socket.socket | None = None
+        self._message_reader = None
         self._serializer = message_serializer
         self._deserializer = message_deserializer
         self._msg_len = NETWORK_MSG_LEN_FORMAT
-        self._msg_bytes = NETWORK_MSG_LEN_FIELD_BYTES
-        self._msg_reader = MessageReader
 
     def connect(self) -> None:
         """
@@ -38,6 +40,8 @@ class NetworkClient:
 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((self._ip, self._port))
+
+        self._message_reader = StreamMessageReader(self._socket.makefile("rb"), self._msg_len)
 
     def send_request(self, request: Request) -> T:
         """
@@ -57,8 +61,9 @@ class NetworkClient:
         length = struct.pack(NETWORK_MSG_LEN_FORMAT, len(data))
         self._socket.sendall(length + data)
 
-        response: T = self._msg_reader.read_message(self._socket.makefile('rb'))
-        return response
+        msg_bytes = self._message_reader.read()
+        return self._deserializer.deserialize(msg_bytes)
+
 
     def close(self) -> None:
         """
