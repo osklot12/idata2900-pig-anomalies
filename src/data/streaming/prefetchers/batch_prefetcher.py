056cf29d657e2b40a2be7783887783da502f1326
Christian Bjerkenes Rø <chrro@ntnu.no>
2025-04-03 20:19:30 +0200

    Edited yoloxi input format

--- src/data/streaming/prefetchers/batch_prefetcher.py
+++ src/data/streaming/prefetchers/batch_prefetcher.py
@@ -1,5 +1,6 @@
 import queue
 import threading
+import time
 from typing import TypeVar, List
 
 from typing_extensions import Generic
@@ -53,6 +54,7 @@ class BatchPrefetcher(Generic[T], Prefetcher[List[T]]):
     def _worker(self) -> None:
         """Worker function that runs on the worker threads."""
         while self._running:
+            time.sleep(.2)
             try:
                 batch = self._provider.get_batch(self._split, self._batch_size)
                 self._queue.put(batch, timeout=.1)
