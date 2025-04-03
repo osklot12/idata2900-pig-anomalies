a1041b6adc6c55c67d111e33633f4847027669d8
Oskar <osklot12@gmail.com>
2025-04-03 16:51:15 +0200

    Fixed prefetcher runner bug

--- src/prefetcher_runner.py
+++ src/prefetcher_runner.py
@@ -4,6 +4,7 @@ from src.network.client.simple_network_client import SimpleNetworkClient
 from src.network.messages.serialization.pickle_message_deserializer import PickleMessageDeserializer
 from src.network.messages.serialization.pickle_message_serializer import PickleMessageSerializer
 from src.network.network_frame_instance_provider import NetworkFrameInstanceProvider
+import time
 
 
 def main():
@@ -13,8 +14,10 @@ def main():
     provider = NetworkFrameInstanceProvider(client)
 
     prefetcher = BatchPrefetcher(provider, DatasetSplit.TRAIN, batch_size=8)
+    prefetcher.run()
 
     while True:
+        time.sleep(.1)
         batch = prefetcher.get()
         print(f"Got batch: {batch}")
 
