3eac9bb785b52da07e860fe95959b769ab3e637d
Oskar <osklot12@gmail.com>
2025-04-03 18:39:27 +0200

    Overriden data loader function

--- src/models/twod/yolo/x/minimal/exp.py
+++ src/models/twod/yolo/x/minimal/exp.py
@@ -1,4 +1,6 @@
 from yolox.exp import Exp as BaseExp
+from torch.utils.data import DataLoader
+from src.models.twod.yolo.x.minimal.dummy_streaming_dataset import DummyStreamingDataset
 
 class Exp(BaseExp):
     def __init__(self):
@@ -13,3 +15,11 @@ class Exp(BaseExp):
         self.print_interval = 1
         self.eval_interval = 1
         self.exp_name = "dummy_exp"
+
+    def get_data_loader(self, batch_size, is_distributed, no_aug=False, cache_img: str = None):
+        return DataLoader(
+            DummyStreamingDataset(num_batches=100),
+            batch_size=batch_size,
+            num_workers=self.data_num_workers,
+            pin_memory=True
+        )
