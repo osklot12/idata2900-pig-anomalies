a507635bfb245129056c503ace1b2c406fcc2085
Oskar <osklot12@gmail.com>
2025-04-03 15:44:00 +0200

    Fixed dataset split bug

--- src/models/twod/yolo/x/train.py
+++ src/models/twod/yolo/x/train.py
@@ -1,6 +1,5 @@
 from src.data.dataset.dataset_split import DatasetSplit
 from src.data.streaming.prefetchers.batch_prefetcher import BatchPrefetcher
-from src.models.twod.yolo.x.trainer import StreamingTrainer
 from src.network.client.simple_network_client import SimpleNetworkClient
 from src.network.messages.serialization.pickle_message_deserializer import PickleMessageDeserializer
 from src.network.messages.serialization.pickle_message_serializer import PickleMessageSerializer
