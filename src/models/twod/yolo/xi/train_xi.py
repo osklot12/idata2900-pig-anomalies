37fc909ada0e1a82c345a88821bd855192e2fda2
EpicDome <chris8br@gmail.com>
2025-04-04 15:30:40 +0200

    Added fake batch prefetcher to avoid server crash

--- src/models/twod/yolo/xi/train_xi.py
+++ src/models/twod/yolo/xi/train_xi.py
@@ -1,11 +1,10 @@
-from src.data.streaming.prefetchers.batch_prefetcher import BatchPrefetcher
+from src.data.streaming.prefetchers.fake_batch_prefetcher import FakeBatchPrefetcher
 from src.network.client.simple_network_client import SimpleNetworkClient
 from src.network.messages.serialization.pickle_message_deserializer import PickleMessageDeserializer
 from src.network.messages.serialization.pickle_message_serializer import PickleMessageSerializer
 from src.network.network_frame_instance_provider import NetworkFrameInstanceProvider
 from src.models.twod.yolo.xi.yoloxi_dataset import UltralyticsDataset
 from src.models.twod.yolo.xi.yoloxi_training_setup import TrainingSetup
-from src.data.dataset.dataset_split import DatasetSplit
 
 
 def main():
@@ -13,7 +12,7 @@ def main():
     client.connect("10.0.0.1")
 
     batch_provider = NetworkFrameInstanceProvider(client)
-    prefetcher = BatchPrefetcher(batch_provider, DatasetSplit.TRAIN, 1, fetch_timeout=60, buffer_size=1)
+    prefetcher = FakeBatchPrefetcher(batch_size=8)
     prefetcher.run()
     dataset = UltralyticsDataset(prefetcher)
 
