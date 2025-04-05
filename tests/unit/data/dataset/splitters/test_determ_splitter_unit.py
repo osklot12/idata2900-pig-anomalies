6ec9afbd99f75201bbef2c158d5f6c9a5c6d01a8
Oskar <osklot12@gmail.com>
2025-04-05 16:02:06 +0200

    Added string adding unit test for DetermSplitter

--- tests/unit/data/dataset/splitters/test_determ_splitter_unit.py
+++ tests/unit/data/dataset/splitters/test_determ_splitter_unit.py
@@ -1,3 +1,5 @@
+from typing import Tuple, List
+
 import pytest
 
 from src.data.dataset.splitters.determ_splitter import DetermSplitter
@@ -11,6 +13,17 @@ def strings():
         "string4", "string5", "string6",
         "string7", "string8", "string9"
     ]
+
+
+def _get_splits(splitters: List[DetermSplitter]) -> Tuple[List[List[str]], List[List[str]]]:
+    """Returns the first and seconds splits for a list of splitters."""
+    first_splits = []
+    second_splits = []
+    for splitter in splitters:
+        first_splits.append(splitter.first_split)
+        second_splits.append(splitter.second_split)
+
+    return first_splits, second_splits
 
 
 @pytest.mark.unit
@@ -46,14 +59,34 @@ def test_split_determinism(strings):
         splitters.append(DetermSplitter(strings))
 
     # assert
-    first_splits = []
-    second_splits = []
-    for splitter in splitters:
-        first_splits.append(splitter.first_split)
-        second_splits.append(splitter.second_split)
+    first_splits, second_splits = _get_splits(splitters)
 
     assert (
             len(set(tuple(s) for s in first_splits)) ==
             len(set(tuple(s) for s in second_splits)) ==
             1
     )
+
+
+@pytest.mark.unit
+def test_add_string(strings):
+    """Tests that adding a string to the splitter will assign it to a split deterministically."""
+    # arrange
+    str_to_add = "string10"
+    splitters = []
+
+    # act
+    for _ in range(100):
+        splitter = DetermSplitter(strings)
+        splitter.add(str_to_add)
+        splitters.append(splitter)
+
+    # assert
+    first_splits, second_splits = _get_splits(splitters)
+
+    first_splits_set = set(tuple(s) for s in first_splits)
+    second_splits_set = set(tuple(s) for s in second_splits)
+
+    assert len(first_splits_set) == len(second_splits_set) == 1
+    assert str_to_add in first_splits_set or str_to_add not in second_splits_set
+    print(second_splits_set)
