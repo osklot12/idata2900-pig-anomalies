8c4ca40d268b98c3421ba8f530b1a4aa60eb7c87
Oskar <osklot12@gmail.com>
2025-04-06 23:52:55 +0200

    Redesigned SequentialStream and unit tested it

--- src/data/streaming/managers/sequential_streamer_manager.py
+++ src/data/streaming/managers/sequential_streamer_manager.py
@@ -2,31 +2,29 @@ import queue
 import threading
 from typing import TypeVar, Generic, Optional
 
-from src.data.dataclasses.streamed_annotated_frame import StreamedAnnotatedFrame
+from src.data.dataset.streams.sequential_stream import SequentialStream
 from src.data.streaming.factories.aggregated_streamer_factory import AggregatedStreamerFactory
-from src.data.streaming.feedables.feedable_queue import FeedableQueue
 from src.data.streaming.managers.concurrent_streamer_manager import ConcurrentStreamerManager
 from src.data.streaming.streamers.streamer import Streamer
 
 T = TypeVar("T")
 
 
-class QueueStreamerManager(Generic[T], ConcurrentStreamerManager):
-    """Manages streamers that streams into queues."""
+class SequentialStreamerManager(Generic[T], ConcurrentStreamerManager):
+    """A streamer manager for directing streamers to a SequentialStream."""
 
-    def __init__(self, streamer_factory: AggregatedStreamerFactory, queue_stream: queue.Queue[queue.Queue[T]],
-                 max_streamers: int = 10):
+    def __init__(self, streamer_factory: AggregatedStreamerFactory, stream: SequentialStream, max_streamers: int = 10):
         """
-        Initializes a QueueStreamerManager instance.
+        Initializes a RoutingStreamerManager instance.
 
         Args:
             streamer_factory (AggregatedStreamerFactory): the factory for creating aggregated streamers
-            queue_stream (queue.Queue(queue.Queue[T])): a stream of queues to get queues from
+            stream (SequentialStream): the sequential stream to feed
             max_streamers (int): the maximum number of concurrent streamers
         """
         super().__init__(max_streamers)
         self._streamer_factory = streamer_factory
-        self._queue_stream: queue.Queue[Optional[queue.Queue[T]]] = queue_stream
+        self._stream = stream
 
         self._worker = None
 
@@ -34,15 +32,13 @@ class QueueStreamerManager(Generic[T], ConcurrentStreamerManager):
         """Worker thread function."""
         while self._running:
             try:
-                q = queue.Queue()
-                self._queue_stream.put(q, timeout=0.1)
-
-                streamer = self._streamer_factory.create_streamer(FeedableQueue[StreamedAnnotatedFrame](q))
+                feedable = self._stream.open_feedable_stream(timeout=0.1)
+                streamer = self._streamer_factory.create_streamer(feedable)
                 if streamer:
                     self._launch_streamer(streamer)
                 else:
-                    self._queue_stream.put(None)
-            except queue.Full:
+                    self._stream.close()
+            except RuntimeError:
                 pass
 
     def _setup(self) -> None:
