59205785fe0f8264fbd7837f29c0a0b1539a8ae5
Oskar <osklot12@gmail.com>
2025-04-08 17:47:33 +0200

    Implemented RandomAccessBlockingPool with unit test suite

--- src/data/dataset/streams/pool_stream.py
+++ src/data/dataset/streams/pool_stream.py
@@ -1,11 +1,29 @@
-from typing import TypeVar, Optional
+from typing import TypeVar, Optional, Generic
 
 from src.data.dataset.streams.stream import Stream
+from src.data.preprocessing.augmentation.augmentor import Augmentor
+from src.data.streaming.feedables.feedable import Feedable
 
 T = TypeVar("T")
 
-class PoolStream(Stream):
+class PoolStream(Generic[T], Stream[T], Feedable[T]):
     """Stream reading randomly from a pool of instances."""
+
+    def __init__(self, pool_size: int = 3000, augmentor: Optional[Augmentor[T]] = None):
+        """
+        Initializes a PoolStream instance.
+
+
+        Args:
+            pool_size (int): the max number of instances in the pool at a time, 3000 by default
+            augmentor (Optional[Augmentor[T]]): the augmentor to use, None by default
+        """
+        self._pool_size = pool_size
+        self._augmentor = augmentor
+
+
+    def feed(self, food: Optional[T]) -> None:
+
 
     def read(self) -> Optional[T]:
         pass
---
+++ tests/unit/data/structures/test_random_access_blocking_pool_unit.py
@@ -0,0 +1,125 @@
+import pytest
+import time
+
+from src.data.structures.random_access_blocking_pool import RandomAccessBlockingPool
+
+
+@pytest.fixture
+def data():
+    """Fixture to provide test data."""
+    return [f"string{i}" for i in range(1000)]
+
+
+@pytest.mark.unit
+def test_get_returns_none_when_empty(data):
+    """Tests that get() returns None when pool is empty."""
+    # arrange
+    pool = RandomAccessBlockingPool[str](maxsize=1000, min_ready=0)
+
+    # act
+    instance = pool.get()
+
+    # assert
+    assert instance is None
+
+
+@pytest.mark.unit
+def test_get_blocks_when_min_ready_is_not_met(data):
+    """Tests that the pool blocks on get() if there are not at least min_ready instances in the pool."""
+    # arrange
+    pool = RandomAccessBlockingPool[str](maxsize=1001, min_ready=1001)
+    for s in data:
+        pool.put(s)
+    timeout = 0.1
+
+    # act
+    start_time = time.time()
+    instance = pool.get(timeout=timeout)
+    end_time = time.time()
+
+    # assert
+    assert instance is None
+    assert end_time - start_time >= timeout
+
+
+@pytest.mark.unit
+def test_get_does_not_block_when_min_ready_is_met(data):
+    """Tests that the pool does not block on get() if there are at least min_ready instances in the pool."""
+    # arrange
+    pool = RandomAccessBlockingPool[str](maxsize=1000, min_ready=999)
+    for s in data:
+        pool.put(s)
+    timeout = 1
+
+    # act
+    start_time = time.time()
+    instance = pool.get(timeout=timeout)
+    end_time = time.time()
+
+    # assert
+    assert instance is not None
+    assert isinstance(instance, str)
+    assert end_time - start_time < timeout
+
+
+@pytest.mark.unit
+def test_put_blocks_on_full_pool(data):
+    """Tests that the put() blocks if the pool is full."""
+    # arrange
+    pool = RandomAccessBlockingPool[str](maxsize=1000, min_ready=0)
+    for s in data:
+        pool.put(s)
+    timeout = 0.1
+
+    # act
+    start_time = time.time()
+    put = pool.put("block", timeout=timeout)
+    end_time = time.time()
+
+    # assert
+    assert not put
+    assert end_time - start_time >= timeout
+
+
+@pytest.mark.unit
+def test_get_removes_instance(data):
+    """Tests that get() removes the returned instance from the pool."""
+    # arrange
+    pool = RandomAccessBlockingPool[str](maxsize=1000, min_ready=0)
+    for s in data:
+        pool.put(s)
+    instances = []
+
+    # act
+    for _ in range(len(data)):
+        instance = pool.get()
+        instances.append(instance)
+
+    last_instance = pool.get()
+
+    # assert
+    assert len(instances) == len(data)
+    assert last_instance is None
+
+
+@pytest.mark.unit
+def test_get_returns_random_instance(data):
+    """Tests that get() returns random instances in different orders."""
+    # arrange
+    n_trials = 10
+    pools = [RandomAccessBlockingPool[str](maxsize=1000, min_ready=0) for _ in range(n_trials)]
+    for pool in pools:
+        for s in data:
+            pool.put(s)
+    all_sequences = []
+
+    # act
+    for pool in pools:
+        instances = []
+        for _ in range(len(data)):
+            instances.append(pool.get())
+        all_sequences.append(instances)
+
+    # assert
+    unique_sequences = set(tuple(seq) for seq in all_sequences)
+    assert len(unique_sequences) > 1
