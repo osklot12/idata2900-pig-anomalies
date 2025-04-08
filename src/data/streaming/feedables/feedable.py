69aaf37209373b10335b75930d25a6329654cb7e
Oskar <osklot12@gmail.com>
2025-04-08 22:40:11 +0200

    Removed timeout and bool return value to Feedable interface

--- src/data/streaming/feedables/feedable.py
+++ src/data/streaming/feedables/feedable.py
@@ -8,15 +8,11 @@ class Feedable(Generic[T], ABC):
     """Interface for feedable objects."""
 
     @abstractmethod
-    def feed(self, food: Optional[T], timeout: Optional[float] = None) -> bool:
+    def feed(self, food: Optional[T]) -> None:
         """
         Feeds some food.
 
         Args:
-            food (Optional[T]): the food to feed, or None if there is no more food
-            timeout (Optional[float]): the time to wait before returning
-
-        Returns:
-            bool: True if successfully fed, False otherwise
+            food (T): the food to feed, or None if there is no more food
         """
         raise NotImplementedError
