c75f974a2daf2f715c1f1f1839acfa56afc06ac8
Oskar <osklot12@gmail.com>
2025-04-13 13:55:39 +0200

    Reworked FrameResizerComponent to fit the Component interface

--- src/data/preprocessing/resizing/resizers/frame_resizer_component.py
+++ src/data/preprocessing/resizing/resizers/frame_resizer_component.py
@@ -1,16 +1,17 @@
 from typing import Optional
 
 from src.data.dataclasses.frame import Frame
+from src.data.dataclasses.streamed_annotated_frame import StreamedAnnotatedFrame
+from src.data.pipeline.component import Component
 from src.data.pipeline.consumer import Consumer
-from src.data.pipeline.producer import Producer
 from src.data.preprocessing.resizing.resizers.frame_resizer import FrameResizer
 from src.data.structures.atomic_var import AtomicVar
 
 
-class FrameResizerComponent(Consumer[Frame], Producer[Frame]):
+class FrameResizerComponent(Component[StreamedAnnotatedFrame]):
     """Pipeline component adapter for frame resizers."""
 
-    def __init__(self, resizer: FrameResizer, consumer: Optional[Consumer[Frame]] = None):
+    def __init__(self, resizer: FrameResizer, consumer: Optional[Consumer[StreamedAnnotatedFrame]] = None):
         """
         Initializes a FrameResizerComponent instance.
 
@@ -19,18 +20,23 @@ class FrameResizerComponent(Consumer[Frame], Producer[Frame]):
             consumer (Optional[Consumer[Frame]]): the consumer of the streamed frames
         """
         self._resizer = resizer
-        self._consumer = AtomicVar[Consumer[Frame]](consumer)
+        self._consumer = AtomicVar[Consumer[StreamedAnnotatedFrame]](consumer)
 
-    def consume(self, data: Optional[Frame]) -> bool:
-        resized = self._resize_frame(data)
+    def consume(self, data: Optional[StreamedAnnotatedFrame]) -> bool:
+        resized = None
+
+        if data is not None:
+            resized = self._resize_frame(data)
+
         return self._consumer.get().consume(resized)
 
-    def connect(self, consumer: Consumer[Frame]) -> None:
+    def connect(self, consumer: Consumer[StreamedAnnotatedFrame]) -> None:
         self._consumer.set(consumer)
 
-    def _resize_frame(self, frame: Frame) -> Frame:
-        return Frame(
-            frame.source,
-            frame.index,
-            self._resizer.resize_frame(frame.data)
+    def _resize_frame(self, instance: StreamedAnnotatedFrame) -> StreamedAnnotatedFrame:
+        return StreamedAnnotatedFrame(
+            source=instance.source,
+            index=instance.index,
+            frame=self._resizer.resize_frame(instance.frame),
+            annotations=instance.annotations
         )
