12813c949a42aff16204b8f52d1ee8c4a787908c
Oskar <osklot12@gmail.com>
2025-04-16 23:06:57 +0200

    Refactored pipeline builder

--- src/data/pipeline/pipeline.py
+++ src/data/pipeline/pipeline.py
@@ -1,17 +1,28 @@
 from typing import Generic, TypeVar
 
 from src.data.pipeline.component import Component
+from src.data.pipeline.pipeline_builder import PipelineBuilder
 from src.data.pipeline.pipeline_tail import PipelineTail
-from src.data.pipeline.producer import Producer
 
+# input type of pipeline
 A = TypeVar("A")
+
+# current output type of pipeline
 B = TypeVar("B")
+
+# next output type
 C = TypeVar("C")
 
-class Pipeline(Generic[A, B, C]):
-    """Pipeline builder class."""
+class Pipeline(Generic[A, B, C], PipelineBuilder[A, B]):
+    """Pipeline builder."""
 
     def __init__(self, head: Component[A, B]):
+        """
+        Initializes a Pipeline instance.
+
+        Args:
+            head (Component[A, B]): the first component of the pipeline
+        """
         self._head = head
 
     def then(self, next_component: Component[B, C]) -> PipelineTail[A, C]:
--- src/data/pipeline/pipeline_tail.py
+++ src/data/pipeline/pipeline_tail.py
@@ -2,13 +2,19 @@ from typing import TypeVar, Generic
 
 from src.data.pipeline.component import Component
 from src.data.pipeline.consumer import Consumer
+from src.data.pipeline.pipeline_builder import PipelineBuilder
 from src.data.pipeline.producer import Producer
 
+# input type of pipeline
 A = TypeVar("A")
+
+# current output type of pipeline
 B = TypeVar("B")
+
+# next output type
 C = TypeVar("C")
 
-class PipelineTail(Generic[A, B]):
+class PipelineTail(Generic[A, B], PipelineBuilder[A, B]):
     """Tail for a pipeline builder."""
 
     def __init__(self, head: Consumer[A], tail: Producer[B]):
@@ -20,5 +26,14 @@ class PipelineTail(Generic[A, B]):
         return PipelineTail(head=self._head, tail=next_component)
 
     def into(self, sink: Consumer[B]) -> "Consumer[A]":
+        """
+        Connects the final consumer and builds the pipeline.
+
+        Args:
+            sink (Consumer[B]): final consumer (sink) of the pipeline
+
+        Returns:
+            Consumer[A]: the first component of the pipeline
+        """
         self._tail.connect(sink)
         return self._head
