028fa2093385f06b1df89e4be9f141e2dcae0b6c
Oskar <osklot12@gmail.com>
2025-04-16 22:25:20 +0200

    Implemented Processor interface

--- src/data/config/config_parser.py
+++
@@ -1,6 +0,0 @@
-from src.data.config.pipeline_config import PipelineConfig
-
-class ConfigParser:
-    @staticmethod
-    def parse(cfg_dict: dict) -> PipelineConfig:
-        return PipelineConfig(**cfg_dict)
