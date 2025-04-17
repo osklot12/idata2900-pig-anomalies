e8bb0463f13a02bb3a0787b58e1398c4028d144d
Oskar <osklot12@gmail.com>
2025-04-17 14:43:07 +0200

    Made InstanceAugmentor a processor, to fit nicely with the new pipeline builder

--- src/data/pipeline/norsvin_train_pipeline_provider.py
+++ src/data/pipeline/norsvin_train_pipeline_provider.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from src.data.compressors.zlib_compressor import ZlibCompressor
 from src.data.dataclasses.annotated_frame import AnnotatedFrame
@@ -9,24 +9,20 @@ from src.data.pipeline.field_transformer import FieldTransformer
 from src.data.pipeline.pipeline import Pipeline
 from src.data.pipeline.preprocessor import Preprocessor
 from src.data.pipeline.splitting_preprocessor import SplittingPreprocessor
-from src.data.preprocessing.augmentation.augmentors.augmentor_component import AugmentorComponent
-from src.data.preprocessing.augmentation.augmentors.instance_augmentor import InstanceAugmentor
-from src.data.preprocessing.augmentation.augmentors.photometric.factories.brightness_filter_factory import \
-    BrightnessFilterFactory
-from src.data.preprocessing.augmentation.augmentors.photometric.factories.color_jitter_filter_factory import \
-    ColorJitterFilterFactory
-from src.data.preprocessing.augmentation.augmentors.photometric.factories.constrast_filter_factory import \
-    ContrastFilterFactory
-from src.data.preprocessing.augmentation.augmentors.photometric.factories.gaussian_noise_filter_factory import \
-    GaussianNoiseFilterFactory
-from src.data.preprocessing.augmentation.cond_multiplier_component import CondMultiplierComponent
+from src.data.preprocessing.augmentation.augmentors.photometric.brightness_filter import BrightnessFilter
+from src.data.preprocessing.augmentation.augmentors.photometric.color_jitter_filter import ColorJitterFilter
+from src.data.preprocessing.augmentation.augmentors.photometric.contrast_filter import ContrastFilter
+from src.data.preprocessing.augmentation.augmentors.photometric.gaussian_noise_filter import GaussianNoiseFilter
+from src.data.preprocessing.augmentation.augmentors.photometric.photometric_filter import PhotometricFilter
 from src.data.preprocessing.augmentation.plan.augmentation_plan_factory import AugmentationPlanFactory
+from src.data.processing.augmentor import Augmentor
 from src.data.processing.class_balancer import ClassBalancer
 from src.data.preprocessing.normalization.normalizers.simple_bbox_normalizer import SimpleBBoxNormalizer
 from src.data.processing.bbox_normalizer_processor import BBoxNormalizerProcessor
 from src.data.processing.cond_multiplier import CondMultiplier
 from src.data.processing.frame_resizer import FrameResizer
 from src.data.structures.atomic_bool import AtomicBool
+from src.data.structures.random_float import RandomFloat
 from src.utils.norsvin_behavior_class import NorsvinBehaviorClass
 
 RESIZE_SHAPE = (640, 640)
@@ -68,7 +64,7 @@ class NorsvinTrainPipelineProvider(ConsumerProvider[AnnotatedFrame]):
             ).then(
                 SplittingPreprocessor(CondMultiplier(n=2, condition=is_annotated))
             ).then(
-                self._create_augmentor()
+                Preprocessor(Augmentor(plan_factory=AugmentationPlanFactory(), filters=self._create_filters()))
             ).then(
                 self._create_compressor()
             ).into(sink)
@@ -76,45 +72,14 @@ class NorsvinTrainPipelineProvider(ConsumerProvider[AnnotatedFrame]):
         return result
 
     @staticmethod
-    def _create_balancer() -> ClassBalancer:
-        """Creates and returns a ClassBalancer instance."""
-        return ClassBalancer(
-            class_counts={
-                NorsvinBehaviorClass.BELLY_NOSING: 1885,
-                NorsvinBehaviorClass.TAIL_BITING: 1073,
-                NorsvinBehaviorClass.EAR_BITING: 1008,
-                NorsvinBehaviorClass.TAIL_DOWN: 1107
-            },
-            max_samples_per=3
-        )
-
-    @staticmethod
-    def _create_multiplier() -> CondMultiplierComponent[AnnotatedFrame]:
-        """Creates and returns a CondMultiplierComponent instance."""
-
-        def is_annotated(instance: AnnotatedFrame) -> bool:
-            """Predicate for checking whether the instance is annotated."""
-            return len(instance.annotations) > 0
-
-        return CondMultiplierComponent(
-            n=2,
-            predicate=is_annotated
-        )
-
-    @staticmethod
-    def _create_augmentor() -> AugmentorComponent:
-        """Creates and returns an AugmentorComponent instance."""
-        return AugmentorComponent(
-            InstanceAugmentor(
-                plan_factory=AugmentationPlanFactory(),
-                filters=[
-                    BrightnessFilterFactory(beta_range=(-30, 30)).create_filter(),
-                    ContrastFilterFactory(alpha_range=(0.8, 1.2)).create_filter(),
-                    ColorJitterFilterFactory(saturation_range=(0.8, 1.2), hue_range=(-10, 10)).create_filter(),
-                    GaussianNoiseFilterFactory(std_range=(5, 15)).create_filter()
-                ]
-            )
-        )
+    def _create_filters() -> List[PhotometricFilter]:
+        """Creates photometric filters for augmentation."""
+        return [
+            BrightnessFilter(beta=RandomFloat(-30, 30)),
+            ContrastFilter(alpha=RandomFloat(0.8, 1.2)),
+            ColorJitterFilter(saturation_scale=RandomFloat(0.8, 1.2), hue_shift=RandomFloat(-10, 10)),
+            GaussianNoiseFilter(std=RandomFloat(5, 15))
+        ]
 
     @staticmethod
     def _create_compressor() -> ZlibCompressor:
