590fea524c3024bcb1fe2521c2cdc0533e424303
Oskar <osklot12@gmail.com>
2025-04-17 17:42:12 +0200

    Implemented SelectorFactory

--- src/utils/norsvin_dataset_config.py
+++ src/utils/norsvin_dataset_config.py
@@ -1,3 +1,12 @@
 from src.data.dataclasses.dataset_split_ratios import DatasetSplitRatios
+from src.utils.gcs_credentials import GCSCredentials
+from src.utils.path_finder import PathFinder
+from tests.utils.gcs.test_bucket import TestBucket
+
+NORSVIN_BUCKET_NAME = "norsvin-g2b-behavior-prediction"
+
+NORSVIN_SERVICE_ACCOUNT_FILE_PATH = str(PathFinder.get_abs_path(".secrets/service-account.json"))
+
+NORSVIN_GCS_CREDS = GCSCredentials(bucket_name=NORSVIN_BUCKET_NAME, service_account_path=NORSVIN_SERVICE_ACCOUNT_FILE_PATH)
 
 NORSVIN_SPLIT_RATIOS = DatasetSplitRatios(0.8, 0.1, 0.1)
--- tests/integration/data/dataset/streams/test_gcs_stream_int.py
+++ tests/integration/data/dataset/streams/test_gcs_stream_int.py
@@ -1,130 +1,22 @@
 import pytest
 import time
 
-from src.data.dataclasses.dataset_split_ratios import DatasetSplitRatios
-from src.data.dataclasses.annotated_frame import AnnotatedFrame
-from src.data.dataset.dataset_split import DatasetSplit
 from src.data.dataset.streams.managed.factories.gcs_stream_factory import GCSStreamFactory
-from src.data.preprocessing.augmentation.augmentors.factories.augmentor_component_factory import \
-    AugmentorComponentFactory
-from src.data.preprocessing.augmentation.augmentors.factories.instance_augmentor_factory import InstanceAugmentorFactory
-from src.data.processing.augmentation.photometric.factories.brightness_filter_factory import \
-    BrightnessFilterFactory
-from src.data.processing.augmentation.photometric.factories import \
-    ColorJitterFilterFactory
-from src.data.processing.augmentation.photometric.factories.constrast_filter_factory import \
-    ContrastFilterFactory
-from src.data.processing.augmentation.photometric.factories import \
-    GaussianNoiseFilterFactory
-from src.data.preprocessing.class_balancer_factory import ClassBalancerFactory
-from src.data.preprocessing.cond_multiplier_component_factory import CondMultiplierComponentFactory
-from src.data.preprocessing.normalization.factories.bbox_normalizer_component_factory import \
-    BBoxNormalizerComponentFactory
-from src.data.preprocessing.normalization.factories.simple_bbox_normalizer_factory import SimpleBBoxNormalizerFactory
-from src.data.preprocessing.resizing.factories.frame_resizer_component_factory import FrameResizerComponentFactory
-from src.data.preprocessing.resizing.factories.static_frame_resizer_factory import StaticFrameResizerFactory
-from src.data.structures.atomic_bool import AtomicBool
-from src.utils.gcs_credentials import GCSCredentials
 from src.utils.norsvin_behavior_class import NorsvinBehaviorClass
-from tests.utils.gcs.test_bucket import TestBucket
-from tests.utils.streamed_annotated_frame_visualizer import StreamedAnnotatedFrameVisualizer
+from src.utils.norsvin_dataset_config import NORSVIN_GCS_CREDS, NORSVIN_SPLIT_RATIOS
 
 
-@pytest.fixture
-def gcs_creds():
-    """Fixture to provide a GCSCredentials instance."""
-    return GCSCredentials(bucket_name=TestBucket.BUCKET_NAME, service_account_path=TestBucket.SERVICE_ACCOUNT_FILE)
-
-
-@pytest.fixture
-def split_ratios():
-    """Fixture to provide a DatasetSplitRatios instance."""
-    return DatasetSplitRatios(train=0.8, val=0.1, test=0.1)
-
-
-@pytest.fixture
-def resizer_component_factory():
-    """Fixture to provide a FrameResizerComponentFactory instance."""
-    frame_resizer_factory = StaticFrameResizerFactory((640, 640))
-    return FrameResizerComponentFactory(frame_resizer_factory)
-
-
-@pytest.fixture
-def normalizer_component_factory():
-    """Fixture to provide a BBoxNormalizerComponentFactory instance."""
-    bbox_normalizer_factory = SimpleBBoxNormalizerFactory((0, 1))
-    return BBoxNormalizerComponentFactory(bbox_normalizer_factory)
-
-@pytest.fixture
-def balancer_factory():
-    """Fixture to provide a CondMultiplierComponentFactory instance."""
-    return ClassBalancerFactory(
-        class_counts={
-            NorsvinBehaviorClass.BELLY_NOSING: 1885,
-            NorsvinBehaviorClass.TAIL_BITING: 1073,
-            NorsvinBehaviorClass.EAR_BITING: 1008,
-            NorsvinBehaviorClass.TAIL_DOWN: 1107
-        },
-        max_samples_per=3
-    )
-
-def is_annotated(instance: AnnotatedFrame) -> bool:
-    """Predicate for checking whether the instance is annotated."""
-    return len(instance.annotations) > 0
-
-@pytest.fixture
-def multiplier_factory():
-    """Fixture to provide a CondMultiplierComponentFactory instance."""
-    return CondMultiplierComponentFactory(
-        n=2,
-        predicate=is_annotated
-    )
-
-@pytest.fixture
-def augmentor_factory():
-    """Fixture to provide a AugmentorComponentFactory instance."""
-    augmentor_factory = InstanceAugmentorFactory(
-        filter_factories=[
-            BrightnessFilterFactory(beta_range=(-30, 30)),
-            ContrastFilterFactory(alpha_range=(0.8, 1.2)),
-            ColorJitterFilterFactory(saturation_range=(0.8, 1.2), hue_range=(-10, 10)),
-            GaussianNoiseFilterFactory(std_range=(5, 15))
-        ]
-    )
-
-    return AugmentorComponentFactory(
-        augmentor_factory=augmentor_factory
-    )
-
-import tracemalloc
-import threading
-
-tracemalloc.start()
-
-
-def report_memory():
-    current, peak = tracemalloc.get_traced_memory()
-    print(f"[Memory] Current = {current / 1024 ** 2:.2f} MB; Peak = {peak / 1024 ** 2:.2f} MB")
-    print(f"[Threads] Active threads: {threading.active_count()}")
-
-
-def test_norsvin_train_stream(gcs_creds, split_ratios, resizer_component_factory, normalizer_component_factory,
-                              balancer_factory, multiplier_factory, augmentor_factory):
+def test_norsvin_train_stream():
     """Tests that creating the Norsvin training set stream with GCSStreamFactory gives the correct stream."""
     # arrange
     stream_factory = GCSStreamFactory(
-        gcs_creds=gcs_creds,
-        split_ratios=split_ratios,
-        split=DatasetSplit.TRAIN,
+        gcs_creds=NORSVIN_GCS_CREDS,
+        split_ratios=NORSVIN_SPLIT_RATIOS,
+        split=
         label_map=NorsvinBehaviorClass.get_label_map(),
-        preprocessor_factories=[
-            resizer_component_factory,
-            normalizer_component_factory,
-            balancer_factory,
-            multiplier_factory,
-            augmentor_factory
-        ]
+        stream_factory=
     )
+
     stream = stream_factory.create_stream()
 
     # act
