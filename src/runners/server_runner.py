4c75a77883498f05e9845acaf8546c4171787599
Oskar <osklot12@gmail.com>
2025-04-18 01:40:32 +0200

    Fixed None receival bug in Prefetcher

--- src/runners/server_runner.py
+++ src/runners/server_runner.py
@@ -108,10 +108,10 @@ def main():
         tracemalloc.start(25)
 
         while True:
-            report_memory()
-            report_objects()
-            mem = proc.memory_info()
-            print(f"[System] RSS={mem.rss / 1024 ** 2:.2f} MB | VMS={mem.vms / 1024 ** 2:.2f} MB")
+            #report_memory()
+            #report_objects()
+            # mem = proc.memory_info()
+            # print(f"[System] RSS={mem.rss / 1024 ** 2:.2f} MB | VMS={mem.vms / 1024 ** 2:.2f} MB")
 
             time.sleep(1)
 
