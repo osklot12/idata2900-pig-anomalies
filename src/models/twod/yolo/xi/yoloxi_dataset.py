215a88489cbead650a5d4ec0a0a552b8878c3d17
EpicDome <chris8br@gmail.com>
2025-04-21 17:00:35 +0200

    Fixed train_xi class

--- src/models/twod/yolo/xi/yoloxi_dataset.py
+++ src/models/twod/yolo/xi/yoloxi_dataset.py
@@ -1,26 +1,19 @@
 from torch.utils.data import IterableDataset
+
+from src.data.dataset.streams.closable_stream import ClosableStream
 from src.models.converters.xi.ultralytics_batch_converter import UltralyticsBatchConverter
-from src.data.streaming.prefetchers.batch_prefetcher import BatchPrefetcher
-import time
+from src.data.dataset.streams.prefetcher import Prefetcher
+from src.data.dataclasses.annotated_frame import AnnotatedFrame
 
 
 class UltralyticsDataset(IterableDataset):
-    """An iterable dataset for Ultralytics OBB training."""
-
-    def __init__(self, prefetcher: BatchPrefetcher, num_batches: int = 6495):
-        """
-        Args:
-            prefetcher (BatchPrefetcher): the prefetcher that streams AnnotatedFrames
-        """
+    def __init__(self, stream: ClosableStream, num_batches: int = 6495):
         super().__init__()
-        self._prefetcher = prefetcher
+        self._stream = stream
         self._num_batches = num_batches
 
     def __iter__(self):
         for _ in range(self._num_batches):
-            batch = self._prefetcher.get()
+            batch = self._stream.read()
             for sample in UltralyticsBatchConverter.convert(batch):
                 yield sample
-
-    def __len__(self):
-        return self._num_batches * getattr(self._prefetcher, "batch_size", 4)
