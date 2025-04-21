6097be9962f98c26a010d89060410e8fc3167711
EpicDome <chris8br@gmail.com>
2025-04-21 17:43:04 +0200

    New fix

--- src/models/twod/yolo/xi/yoloxi_dataset.py
+++ src/models/twod/yolo/xi/yoloxi_dataset.py
@@ -6,21 +6,17 @@ from typing import List
 
 
 class UltralyticsDataset(IterableDataset):
-    def __init__(self, prefetcher: Prefetcher[AnnotatedFrame], num_batches: int = 6495, batch_size: int = 8):
+    def __init__(self, prefetcher: Prefetcher[AnnotatedFrame], batch_size: int = 8, num_batches: int = 6495):
         super().__init__()
         self._prefetcher = prefetcher
-        self._num_batches = num_batches
         self._batch_size = batch_size
+        self._num_batches = num_batches
 
     def __iter__(self):
         for _ in range(self._num_batches):
-            frames: List[AnnotatedFrame] = []
-            for _ in range(self._batch_size):
-                frame = self._prefetcher.read()
-                frames.append(frame)
-
-            for sample in UltralyticsBatchConverter.convert(frames):
+            batch = [self._prefetcher.read() for _ in range(self._batch_size)]
+            for sample in UltralyticsBatchConverter.convert(batch):
                 yield sample
 
     def __len__(self):
-        return self._num_batches
+        return self._num_batches * self._batch_size
