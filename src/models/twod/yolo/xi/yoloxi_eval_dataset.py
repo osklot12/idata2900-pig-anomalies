d8a7ec3474360a155d1b585064d3a5b7ef7d75c0
EpicDome <chris8br@gmail.com>
2025-04-21 17:27:43 +0200

    fixed dataset

--- src/models/twod/yolo/xi/yoloxi_eval_dataset.py
+++ src/models/twod/yolo/xi/yoloxi_eval_dataset.py
@@ -1,24 +1,26 @@
 from torch.utils.data import IterableDataset
-
-from src.data.dataset.streams.closable_stream import ClosableStream
 from src.models.converters.xi.ultralytics_batch_converter import UltralyticsBatchConverter
 from src.data.dataset.streams.prefetcher import Prefetcher
-from src.data.dataclasses.annotated_frame import AnnotatedFrame
+from src.data.dataclasses.annotated_frame import AnnotatedFrame  # needed for type hint
+from typing import List
 
 
 class EvalUltralyticsDataset(IterableDataset):
-    """A finite iterable dataset for Ultralytics OBB evaluation."""
-
-    def __init__(self, stream: ClosableStream, num_batches: int = 6495):
+    def __init__(self, prefetcher: Prefetcher[AnnotatedFrame], num_batches: int = 6495, batch_size: int = 8):
         super().__init__()
-        self._stream = stream
+        self._prefetcher = prefetcher
         self._num_batches = num_batches
+        self._batch_size = batch_size
 
     def __iter__(self):
         for _ in range(self._num_batches):
-            batch = self._stream.read()
-            for sample in UltralyticsBatchConverter.convert(batch):
+            frames: List[AnnotatedFrame] = []
+            for _ in range(self._batch_size):
+                frame = self._prefetcher.read()
+                frames.append(frame)
+
+            for sample in UltralyticsBatchConverter.convert(frames):
                 yield sample
 
-    def __len__(self):  # <- This is the critical fix
+    def __len__(self):
         return self._num_batches
