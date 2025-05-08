3be1cbb49b5fd270d81fd4b9a834d1f4b4942a2e
Oskar <osklot12@gmail.com>
2025-05-08 20:19:27 +0200

    Configured YOLOX for full training

--- src/models/twod/yolo/x/streaming_exp.py
+++ src/models/twod/yolo/x/streaming_exp.py
@@ -59,7 +59,7 @@ class StreamingExp(BaseExp):
         dataset = StreamingDataset(
             stream_provider=self._train_stream_provider,
             batch_size=28,
-            n_batches=1 # 267
+            n_batches=267
         )
         return DataLoader(
             dataset=dataset,
