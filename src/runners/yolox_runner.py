9871bf96c5c4f57a9ed85e8f3a0e24688b3d8d76
Oskar <osklot12@gmail.com>
2025-05-09 15:01:59 +0200

    Started YOLOX training from scratch

--- src/runners/yolox_runner.py
+++ src/runners/yolox_runner.py
@@ -50,7 +50,7 @@ def main():
         machine_rank=0,
         dist_url="auto",
         experiment_name=exp.exp_name,
-        ckpt="YOLOX_outputs/streaming_yolox/epoch_37_ckpt.pth",
+        ckpt="YOLOX_weights/yolox_s.pth",
         fp16=True,
         fuse=False,
         cache=False,
